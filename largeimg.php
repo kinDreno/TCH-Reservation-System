f08cca901e88c973e250600c2b985a1008859083
quino_kino <147179328+QuinSue@users.noreply.github.com>
2024-03-06 10:38:00 +0800

    Add files via upload

---
+++ largeimg.php
@@ -0,0 +1,34 @@
+  
+            <!-- Contact Details Column -->
+            <div class="col-lg-8">
+                <h3>Contact Details</h3>
+                <p>
+                    Olivarez Plaza<br>E. Aguinaldo Hi-way, Tagaytay City, Philippines<br>
+                </p>
+                <p><i class="fa fa-phone"></i> 
+                    <abbr title="Phone">P</abbr>: (046) 413 3301</p>
+                <p><i class="fa fa-envelope-o"></i> 
+                    <abbr title="Email">E</abbr>: <a href="tch.inquiry@gmail.com">tch.inquiry@gmail.com</a>
+                </p>
+                <p><i class="fa fa-clock-o"></i> 
+                    <abbr title="Hours">H</abbr>: Monday - Friday: 9:00 AM to 5:00 PM</p>
+                <ul class="list-unstyled list-inline list-social-icons">
+                    <li>
+                        <a href="https://www.facebook.com/TagaytayCountryHotel?mibextid=ZbWKwL"><i class="fa fa-facebook-square fa-2x"></i>facebook</a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="fa fa-linkedin-square fa-2x"></i></a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="fa fa-twitter-square fa-2x"></i></a>
+                    </li>
+                    <li>
+                        <a href="tch.inquiry@gmail.com"><i class="fa fa-google-plus-square fa-2x"></i></a>
+                    </li>
+                </ul>
+            </div>
+              <div class="col-lg-4"> 
+        <div class="row">
+          <?php  require_once('sidebar.php') ; ?>
+        </div>
+      </div>
